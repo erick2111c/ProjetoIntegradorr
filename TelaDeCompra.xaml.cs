6f611c48fc1916510c84b350e8152759ee6e27d3
VICAEL VALENTIM KUHN <04287731093@senacrs.edu.br>
2025-11-26 21:07:25 -0300

    Update

--- TelaDeCompra.xaml.cs
+++ TelaDeCompra.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void btnComprar1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
